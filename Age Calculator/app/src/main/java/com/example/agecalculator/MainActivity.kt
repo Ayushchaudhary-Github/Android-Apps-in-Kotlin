a3551cb6acf4a562ec914adf045661ae2810d12d
Ayushchaudhary-Github <chaudharyayush467@gmail.com>
2023-04-09 18:13:03 +0530

    date picker tested by btn1

--- Age Calculator/app/src/main/java/com/example/agecalculator/MainActivity.kt
+++ Age Calculator/app/src/main/java/com/example/agecalculator/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.agecalculator
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,7 +14,26 @@ class MainActivity : AppCompatActivity() {
         val btn : Button = findViewById(R.id.button1)
 
         btn.setOnClickListener {
-            Toast.makeText(this,"btn pressed", Toast.LENGTH_LONG).show()
+            clickbtn()
         }
+
+        }
+
+    fun clickbtn(){
+
+            val myCalendar = Calendar.getInstance()
+            val year = myCalendar.get(Calendar.YEAR)
+            val month = myCalendar.get(Calendar.MONTH)
+            val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+
+        DatePickerDialog(this, DatePickerDialog.OnDateSetListener{ view, year, month, dayOfMonth ->
+            Toast.makeText(this,"btn pressed", Toast.LENGTH_LONG).show()
+        },
+        year,
+        month,
+        day
+        ).show()
+
     }
+
 }
